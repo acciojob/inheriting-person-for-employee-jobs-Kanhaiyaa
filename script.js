1458c4860674f8b61d8faa556f140963d760a5e8
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-16 10:56:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	person.prototype.greet=function(){
+		console.log(`my name is ${name}, I am ${age} years old`)
+	}
+}
 
-function Employee(name, age, jobTitle) {}
 
+
+function Employee(name, age, jobTitle) {
+	Person.call(this,name,age)
+	this.jobTitle=jobTitle
+}
+
+Employee.prototype=Object.create(Person.prototype);
+Employee.prototype.constructor=Employee
+
+var emp1=new Employee("luffy",23,"kaizokoni worewa")
+console.log(emp1)
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
